68720a969f161ab5c8da41c2daf310837610732a
AShalyuta <58889437+AShalyuta@users.noreply.github.com>
2021-12-09 05:39:09 +0300

    vue

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <Header />
+    <router-view />
+  </div>
+</template>
+
+<script>
+import Header from "./components/Header.vue";
+
+export default {
+  name: "App",
+  components: {
+    Header,
+  },
+};
+</script>
+
+<style lang="scss">
+html,body {
+    margin: 0;
+    padding: 0;
+   height: 100%;
+ }
+  #app {
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    align-items: center;
+    min-width: 720px;
+    min-height: 100%;
+  }
+</style>
